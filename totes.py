7ef13faf74decb5b54ff8fad91eb85735f9bc054
kcp288 <kira.prentice@nyu.edu>
2016-02-06 16:26:43 -0500

    mostly functional

--- totes.py
+++ totes.py
@@ -4,19 +4,25 @@ vowels = {'AA0': 0, 'AA2': 1, 'AE0': 2, 'AE2': 3, 'AH0': 4, 'AH2': 5, 'AO0': 6, 
 
 consonants = {'B': 0, 'CH': 1, 'D': 2, 'DH': 3, 'F': 4, 'G': 5, 'HH': 6, 'JH': 7, 'K': 8, 'L': 9, 'M': 10, 'N': 11, 'NG': 12, 'P': 13, 'R': 14, 'S': 15, 'SH': 16, 'T': 17, 'TH': 18, 'V': 19, 'W': 20, 'Y': 21, 'Z': 22, 'ZH': 23}
 
-v_cons = {'D': 2, 'DH': 3, 'G': 5, 'JH': 7, 'M': 10, 'N': 11, 'NG': 12, 'R': 14, 'V': 19, 'W': 20, 'Y': 21, 'Z': 22, 'ZH': 23}
+v_cons = {'D': 2, 'DH': 3, 'G': 5, 'JH': 7, 'M': 10, 'N': 11, 'NG': 12, 'R': 14, 'V': 19, 'W': 20, 'Y': 21}
 
-nv_cons = {'B': 0, 'F': 4, 'G': 5, 'K': 8, 'P': 13, 'S': 15, 'SH': 16, 'T': 17, 'TH': 18, 'Y': 21}
+nv_cons = {'B': 0, 'F': 4, 'G': 5, 'K': 8, 'P': 13, 'T': 17, 'TH': 18, 'Y': 21}
+
+written_consonants = {'B': 0, 'C': 1, 'D': 2, 'F': 4, 'G': 5, 'H': 6, 'J': 7, 'K': 8, 'L': 9, 'M': 10, 'N': 11, 'P': 13, 'Q': 3, 'R': 14, 'S': 15, 'T': 17, 'V': 19, 'W': 20, 'Y': 21, 'Z': 22, 'X': 23}
+
 
 def abbrev(inp):
+  # To uppercase, split into words
   inp = inp.upper()
   inp = inp.split(' ')
+
   output = []
 
-
+  # For each word, call lookup
   for word in inp:
  	abbrev = lookup(word)
 
+ 	print word
  	print abbrev
 
   #print output
@@ -31,59 +37,94 @@ def lookup(token):
   	if token in line:
   	  line = line.split()
   	  if token == line[0]:
-  	  	stressed = False
-  	  	for phone in line:
-  	  		
-  	  		if '1' in phone:
-  	  			stressed = True
-  	  			ndx = line.index(phone)
-  	  			abbrev = line[1:ndx+1]
-
-  	  			newline = line[ndx+1:]
-
-  	  			# No need to pad the coda
-  	  			if len(newline) < 1:
-				  cmu.close()
-  				  return abbrev
-
-  	  			# Else if it is truncated
-  	  			for sound in newline:
-  	  				if sound in vowels:
-  	  					break
-  	  				if sound in consonants:
-  	  					ndx = newline.index(sound)
-
-  	  			coda = newline[0:ndx+1]
-  	  			abbrev = abbrev + coda
-  	  			abbrev = generatecoda(abbrev)
-
-  	  	if stressed == False:
-  	  	  abbrev = line
+  	  	abbrev = findstressed(line, token)
 
 
   if len(abbrev)==0:
 	print token + " not found"
 	return
   
-  
   cmu.close()
   return abbrev
 
-'''
-  	  if token == line[0]:
-  	  	print line
-  	  	for phone in line:
-  	  		if '1' in phone:
-  	  			print phone
-  	  	return token
-  	  	
-  	  for phone in line: 
-	    if '1' in phone:
-	      print line
-	      print phone
+# Get spelling up to stressed syllable
+# Cut off all consonants after it
 
-'''
+def spelling(token, ctr, r):
 
+	vowelcount = 0
+	word = list(token)
+	print word
+	for letter in word:
+		if letter in written_consonants:
+			continue
+		else:
+			vowelcount = vowelcount + 1
+			if vowelcount == ctr:
+				ndx = word.index(letter)
+				break
+
+	# Account for rhotic vowel
+	if r:
+		return  word[0:ndx+2]
+
+	return  word[0:ndx+1]
+
+
+def findstressed(line, token):
+	abbrev = ''
+	numvowels = 0
+	stressedlocation = 0
+
+	for phone in line:
+		if phone in pri_vowels:
+			stressed = True
+
+			# Locate stressed syllable
+			ndx = line.index(phone)
+
+			abbrev = line[1:ndx+1]
+			newline = line[ndx+1:]
+
+			# No need to pad the coda
+			if len(newline) < 1:
+				return abbrev
+
+			# Else if it is truncated
+			for sound in newline:
+				if sound in vowels:
+					break
+				if sound in consonants:
+					ndx = newline.index(sound)
+
+			coda = newline[0:ndx+1]
+			coda2 = generatecoda(abbrev)
+
+			newspelling = ''.join([str(x) for x in coda]) + ''.join([str(x) for x in coda2])
+
+			abbrev = abbrev + coda + coda2
+
+			numvowels = numvowels + 1
+			stressedlocation = numvowels
+			
+		elif phone in vowels:
+			numvowels = numvowels + 1
+
+	rhotic = False
+	for i in abbrev:
+		if i == 'ER1':
+			rhotic = True
+
+	firstpart = spelling(token, stressedlocation, rhotic)
+
+	firstpart = ''.join([str(x) for x in firstpart])
+	print "New Spelling: ", firstpart+newspelling
+
+
+	if ndx == 0:
+		abbrev = line
+
+	return abbrev
 
 def generatecoda(abbrev):
 	if len(abbrev) == 0:
@@ -98,7 +139,7 @@ def generatecoda(abbrev):
 	if end in nv_cons:
 		coda = ['S']
 
-	return abbrev + coda
+	return coda
   
    
 def main():
